248a14f0996215e7b4295281f6a409dade4af1b3
hnimnn <minhbaro@gmail.com>
2023-09-20 22:03:09 +0700

    updated format

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.scss";
-import logoHeader from "../../Assets/SVG/logoHeader.svg";
-import { IconButton,  } from "@mui/material";
+import logoHeader from "../../Assets/icons/logoHeader.svg";
+import { IconButton, } from "@mui/material";
 import Button from "./../Button/Button";
 import { WalletOutlined, Email, Notifications, Subject } from "@mui/icons-material";
 import { grey } from '@mui/material/colors';
@@ -10,43 +10,43 @@ export default function Header() {
   return (
     <div className="wrapper-header">
       <div className="menu">
-        <IconButton  className="menu-btn">
-          <Subject sx={{ color: grey[900], fontSize: 26 }}/>
+        <IconButton className="menu-btn">
+          <Subject sx={{ color: grey[900], fontSize: 26 }} />
         </IconButton>
         <img src={logoHeader} alt="Logo" className="logo" />
       </div>
-    <div className="content">
-    <nav className="nav">
-      <ul>
-        <li><a href="/community">Community</a></li>
-        <li><a href="/recruit">Recruit</a></li>
-        <li><a href="/market">Market</a></li>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/info">Info</a></li>
-      </ul>
-    </nav>
-    <div className="btn-container"> 
-      <Button 
-        onClick={() => console.log("Cook")}
-        icon= {<WalletOutlined style={{fontSize:"22px"}}/>}
-        content = "Wallet Connect"
-        className="btn-responsive-PC btn-wallet"
-        />
-    <Button 
-        onClick={() => console.log("Cook")}
-        icon= {<WalletOutlined style={{fontSize:"17px"}}/>}
-        content = "Wallet"
-        className="btn-responsive-mobile btn-wallet"
-        />
-    <IconButton aria-label="email">
-      <Email sx={{ color: grey[900], fontSize: 17 }}/>
-    </IconButton>
-    <IconButton aria-label="notification">
-      <Notifications sx={{ color: grey[900], fontSize: 17 }}/>
-    </IconButton>
-    <Avatar className="avatar" alt="Remy Sharp" src="https://mpost.io/wp-content/uploads/image-7-17.jpg" />
+      <div className="content">
+        <nav className="nav">
+          <ul>
+            <li><a href="/community">Community</a></li>
+            <li><a href="/recruit">Recruit</a></li>
+            <li><a href="/market">Market</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/info">Info</a></li>
+          </ul>
+        </nav>
+        <div className="btn-container">
+          <Button
+            onClick={() => console.log("Cook")}
+            icon={<WalletOutlined style={{ fontSize: "22px" }} />}
+            content="Wallet Connect"
+            className="btn-responsive-PC btn-wallet"
+          />
+          <Button
+            onClick={() => console.log("Cook")}
+            icon={<WalletOutlined style={{ fontSize: "17px" }} />}
+            content="Wallet"
+            className="btn-responsive-mobile btn-wallet"
+          />
+          <IconButton aria-label="email">
+            <Email sx={{ color: grey[900], fontSize: 17 }} />
+          </IconButton>
+          <IconButton aria-label="notification">
+            <Notifications sx={{ color: grey[900], fontSize: 17 }} />
+          </IconButton>
+          <Avatar className="avatar" alt="Remy Sharp" src="https://mpost.io/wp-content/uploads/image-7-17.jpg" />
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
   );
 }
