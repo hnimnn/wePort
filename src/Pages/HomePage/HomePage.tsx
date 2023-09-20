e03759a988dbc7461ce0088da1910145b22677fa
hnimnn <minhbaro@gmail.com>
2023-09-20 21:54:14 +0700

    update homepage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./HomePage.scss";
 import Header from "../../Components/Header/Header";
-import Banner from "../../Assets/IMG/Banner.png";
+import banner from "../../Assets/IMG/Banner.png";
 import ArrowDown from "../../Assets/SVG/ArrowDown.svg"
 const HomePage = () => {
   return (
     <>
-    <Header/>
-    <div className="wrapper-homepage">
-      <span className="title">The Perfect Place For UI/UX Talent and Enterprises! with Quality Services</span>
-      <img src={Banner} alt="Banner" className="banner"/> 
-    </div>
-    {/* <img src={Banner} alt="Banner" className="banner-scroll"/>
+      <Header />
+      <div className="wrapper-homepage">
+        <section className='banner'>
+
+          <span className="title">The Perfect Place For UI/UX Talent and Enterprises! with Quality Services</span>
+          <img src={banner} alt="Banner" className="banner-img" />
+        </section>
+      </div>
+      {/* <img src={Banner} alt="Banner" className="banner-scroll"/>
     <span className="title-scroll-line1">Let’s Take A Look</span>
     <span className="title-scroll-line2">SHALL WE!</span>
     <img src={ArrowDown} alt="arrow" className="arrow" /> */}
