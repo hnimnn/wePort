69bd258dffa05495964e4d4f9ffdf52e44d0eb13
hnimnn <minhbaro@gmail.com>
2023-09-22 02:53:17 +0700

    banner animation + responsive

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import "./HomePage.scss";
 import Header from "../../Components/Header/Header";
-import banner from "../../Assets/images/Banner.png";
-import ArrowDown from '../../Components/Icons/ArrowDown';
+import Banner from '../../Components/HomePage/Banner/Banner';
 const HomePage = () => {
   return (
     <>
       <Header />
       <div className="wrapper-homepage">
-        <section className='banner'>
-
-          <span className="title">The Perfect Place For UI/UX Talent and Enterprises! with Quality Services</span>
-          <img src={banner} alt="Banner" className="banner-img" />
-          <ArrowDown />
-        </section>
+        <Banner />
       </div>
       {/* <img src={banner} alt="Banner" className="banner-scroll" />
       <span className="title-scroll-line1">Let’s Take A Look</span>
